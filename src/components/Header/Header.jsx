5fae2871f09e40c4c19fc5a33912822811952d81
andreybyrsa <byrsa2004@bk.ru>
2022-12-28 18:15:00 +0300

    Сделал компонент хедер

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import Typography from '../Typography/Typography';
+import TypographyVariantsTypes from '../Typography/TypographyVariants.types';
+import { tg, be, vk } from '../../assets/images';
+import './Header.scss';
+
+const links = [
+  {id: 1, text: 'Создание сайтов'},
+  {id: 2, text: 'Продвижение'},
+  {id: 3, text: 'SMM'},
+  {id: 4, text: 'Тех. поддержка'},
+  {id: 5, text: 'Кейсы'},
+  {id: 6, text: 'Портфолио'},
+  {id: 7, text: 'Контакты'},
+];
+
+function Header({
+  className,
+}) {
+  const HeaderClassName = classNames(
+    'header',
+    className,
+  );
+  return (
+    <div className={HeaderClassName}>
+      <div className="header-content__wrapper">
+        <div className="header__navigation">
+          <div className="header__navigation-logo">
+            <Typography
+              variant={TypographyVariantsTypes.Logo}
+              color="#ffffff"
+            >
+              IT Clan
+            </Typography>
+          </div>
+          <div className="header__navigation-links">
+            {links.map((elem) => {
+              return (
+                <Typography
+                  className="header__navigation-link"
+                  key={elem.id}
+                  color="#ffffff"
+                  variant={TypographyVariantsTypes.Regular_16_400_16}
+                >
+                  {elem.text}
+                </Typography>
+              );
+            })}
+          </div>
+        </div>
+        <div className="header__icons">
+          <img className="header__icon" src={tg} alt="tg"/>
+          <img className="header__icon" src={be} alt="be"/>
+          <img className="header__icon" src={vk} alt="vk"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Header.defaultProps = {
+  className: '',
+};
+
+Header.propTypes = {
+  className: PropTypes.string,
+};
+
+export default Header;
