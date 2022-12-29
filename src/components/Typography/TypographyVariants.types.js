39f8fa833f5dd204a069da0bd26c7afb729468ae
andreybyrsa <byrsa2004@bk.ru>
2022-12-29 13:30:00 +0300

    Настроил типографию и шрифты

--- src/components/Typography/TypographyVariants.types.js
+++ src/components/Typography/TypographyVariants.types.js
@@ -1,8 +1,9 @@
 const TypographyVariantsTypes = {
   Regular_16_400_16: 'regular-16-400-16',
-  Regular_16_700_16: 'regular-16-700-16',
-  Regular_16_400_24: 'regular-16-400-24',
-  Logo: 'logo',
+  Regular_16_400_24: 'regular-16-700-16',
+  Bold_16_700_16: 'bold-16-700-16',
+  Heading_logo: 'heading-logo',
+  Heading: 'heading',
 };
 
 export default TypographyVariantsTypes;
